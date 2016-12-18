cfca5deffbd3157530706cac87e627b750997b69
Klaudia Przespolewska <klaudia1000@onet.eu>
2016-12-18 14:43:16 +0100

    p

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 $(function() {
+    
     $(window).scroll(function(){
      
 /*robie pętle żeby po scrollowaniu 50px ma zmienić kolor */
         if ($(window).scrollTop() >= 50){
+            
             $("#main-nav").addClass('scroll');
+            
         }else {
+            
             $("#main-nav").removeClass('scroll');
         }
     });
+    
+    var root = $('html, body');
+    $('a[href^="#"]').click(function() {
+        
+        //console.log("kliknięcie");
+        
+        var href = $(this).attr("href");
+        root.animate({scrollTop : $(href).offset().top -52}, 500);
+        
+    });
+        
    
 });
